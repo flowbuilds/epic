f236fc375080f9851d99c39773f264d69267dfc6
FlowBuilds <83973323+flowbuilds@users.noreply.github.com>
2021-12-19 23:01:22 +0000

    Update and rename epicCart14.js to epicCart15.js

--- epicCart15.js
+++ epicCart15.js
@@ -34,6 +34,8 @@ if(typeof epic !== "undefined") {
 			epic.cart.ref.product.every((product, i) => {
 				epic.js.ref(epic.js.array(product.el.querySelectorAll("[epic-cart-element]"))).every(item => {
 					console.log(item);
+					if(item.hasOwnProperty("product")) {return true}
+					item.product = epic.cart.ref.product[i]
 					return true
 				});
 				return true
