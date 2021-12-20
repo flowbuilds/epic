90c38d736e46f50fc297a63320f372d2318ced39
FlowBuilds <83973323+flowbuilds@users.noreply.github.com>
2021-12-20 16:48:51 +0000

    Update and rename epicCart19.js to epicCart20.js

--- epicCart20.js
+++ epicCart20.js
@@ -2,6 +2,89 @@
 
 if(typeof epic !== "undefined") {
 	epic.cart = {
+		/*"getproduct": (id) => {
+			if(id === undefined) {return}
+			if(typeof id !== "string") {return}
+			if(!epic.cart.ref.hasOwnProperty("product")) {return}
+			let x;
+			epic.cart.ref.product.every((p, i) => {
+				if(p.id === id) {
+					x = epic.cart.ref.product[i];
+					return false
+				}
+				return true
+			});
+			return x
+		},*/
+		"addtocart": (product) => {
+			if(product === undefined) {return}
+			if(typeof product !== "string") {return}
+			product = epic.cart.getproduct(product);
+			if(product === undefined) {return}
+			console.log(product);
+			let obj = {
+				"name": product.id,
+				"catalogid": "",
+				"price": "",
+				"quantity": "1"
+			}
+			console.log(obj);
+			// collate options + inputs
+			// add to the cart as a new item OR update existing
+			// open cart modal
+		},
+		"input": (x, input) => {
+			if(x === undefined) {return}
+			if(typeof x !== "string") {return}
+			if(input === undefined) {return}
+			if(typeof input !== "object") {return}
+			input = epic.js.ref(input);
+			if(!input.hasOwnProperty("name")) {return}
+			if(!input.hasOwnProperty("product")) {return}
+			let value = input.el.value;
+			if(input.hasOwnProperty("value")) {value = input.value}
+			input.product[input.name] = value
+		},
+		"setinputs": () => {
+			if(!epic.cart.ref.hasOwnProperty("input")) {return}
+			if(!epic.cart.ref.hasOwnProperty("product")) {return}
+			epic.cart.ref.input.every(input => {
+				if(!input.hasOwnProperty("product")) {return true}
+				if(!input.hasOwnProperty("name")) {return true}
+				// populate options
+				if(input.hasOwnProperty("options")) {
+					let options, o = input.options;
+					if(typeof o === "string") {
+						if(o === "variations") {o = "variation"}
+						if(epic.cart.ref.hasOwnProperty(o)) {
+							options = epic.cart.ref[o]
+						}
+					}
+					if(options !== undefined) {
+						// select
+						if(input.el.tagName === "SELECT") {
+							options.every(option => {
+								for(let i = 0; i < input.el.options.length; i++) {
+									if(input.el.options[i].text === option[input.name]) {
+										return true
+									}
+								}
+								o = document.createElement("option");
+								o.text = option[input.name];
+								if(option.hasOwnProperty("quantity")) {
+									if(option.quantity === 0 || option.quantity === "") {
+										o.setAttribute("disabled", "")
+									}
+								}
+								input.el.add(o);
+								return true
+							})
+						}
+					}
+				}
+				return true
+			})
+		},
 		"setoptions": () => {
 			if(!epic.cart.ref.hasOwnProperty("option")) {return}
 			if(!epic.cart.ref.hasOwnProperty("variation")) {return}
@@ -29,103 +112,6 @@ if(typeof epic !== "undefined") {
 				return true
 			})
 		},
-		"getvariation": () => {
-			//
-		},
-		"getproduct": (id) => {
-			if(id === undefined) {return}
-			if(typeof id !== "string") {return}
-			if(!epic.cart.ref.hasOwnProperty("product")) {return}
-			let x;
-			epic.cart.ref.product.every((p, i) => {
-				if(p.id === id) {
-					x = epic.cart.ref.product[i];
-					return false
-				}
-				return true
-			});
-			return x
-		},
-		"updateproduct": (input) => {
-			if(input === undefined) {return}
-			if(typeof input !== "object") {return}
-			if(!input.hasOwnProperty("product")) {return}
-			let value = input.el.value;
-			if(input.hasOwnProperty("value")) {
-				value = input.value
-			}
-			//
-		},
-		"addtocart": (product) => {
-			if(product === undefined) {return}
-			if(typeof product !== "string") {return}
-			product = epic.cart.getproduct(product);
-			if(product === undefined) {return}
-			console.log(product);
-			let obj = {
-				"name": product.id,
-				"catalogid": "",
-				"price": "",
-				"quantity": "1"
-			}
-			console.log(obj);
-			// collate options + inputs
-			// add to the cart as a new item OR update existing
-			// open cart modal
-		},
-		"input": (x, input) => {
-			console.log(x);
-			console.log(input);
-			console.log(this);
-			if(input === undefined) {return}
-			if(typeof input !== "object") {return}
-			input = epic.js.ref(input);
-			console.log(input);
-			if(!input.hasOwnProperty("name")) {return}
-			if(!input.hasOwnProperty("product")) {return}
-			let value = input.el.value;
-			if(input.hasOwnProperty("value")) {value = input.value}
-			input.product[input.name] = value
-		},
-		"setinputs": () => {
-			if(!epic.cart.ref.hasOwnProperty("input")) {return}
-			epic.cart.ref.input.every(input => {
-				if(!input.hasOwnProperty("name")) {return true}
-				if(input.hasOwnProperty("product")) {
-					input.el.addEventListener("input", () => {
-
-					})
-				}
-				return true
-			})
-		},
-		"setinputs": () => {
-			if(!epic.cart.ref.hasOwnProperty("input")) {return}
-			// input.el // input.name // input.options="variations"
-			epic.cart.ref.input.every(input => {
-				if(!input.hasOwnProperty("name")) {return true}
-				// set up updateproduct()
-				// populate options
-				if(input.hasOwnProperty("options")) {
-					let options = [];
-					if(input.options === "variations") {
-						if(epic.cart.ref.hasOwnProperty("variation")) {
-							epic.cart.ref.variation.every(vari => {
-								if(!vari.hasOwnProperty(input.name)) {return true}
-								options.push(vari[input.name])
-								return true
-							})
-						}
-					}
-					if(options.length >= 1) {
-						if(input.el.tagName === "SELECT") {
-							//
-						}
-					}
-				}
-				return true
-			})
-		},
 		"setproducts": () => {
 			if(!epic.cart.ref.hasOwnProperty("product")) {return}
 			epic.cart.ref.product.every((product, i) => {
@@ -143,8 +129,8 @@ if(typeof epic !== "undefined") {
 	}
 	epic.js.refBuilder("cart");
 	if(epic.cart.hasOwnProperty("ref")) {
-		epic.cart.setoptions();
+		// epic.cart.setoptions();
 		epic.cart.setproducts();
-		//epic.cart.setinputs()
+		epic.cart.setinputs()
 	}
 }
