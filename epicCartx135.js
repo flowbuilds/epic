649d2edfc5e3fb5ea53a573d1c87362054951a8a
FlowBuilds <83973323+flowbuilds@users.noreply.github.com>
2022-01-16 23:38:53 +0000

    Update and rename epicCartx134.js to epicCartx135.js

--- epicCartx135.js
+++ epicCartx135.js
@@ -459,11 +459,17 @@ epic.cart = {
 		});
 		console.log(disable);
 		for(group in disable) {
+			console.log("GROUP (IN DISABLE) = " + group);
 			disable[group].forEach(name => {
+				console.log("DISABLE[GROUP] = NAME = " + name);
 				epic.cart.ref.option.forEach(option => {
+					console.log("OPTION.NAME = " + option.name);
 					if(option.name === group) {
+						console.log("MATCH");
 						for(let i = 0; i < option.el.options.length; i++) {
+							console.log("OPTIONS[I].TEXT = " + option.el.options[i].text);
 							if(option.el.options[i].text === name) {
+								console.log("MATCH");
 								option.el.options[i].setAttribute("disabled", "")
 							}
 						}
